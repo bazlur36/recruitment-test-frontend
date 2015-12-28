8f0e9b8ade8b7ed9097117e040d69f019f94cddd
the bazlur <mailtobazlur@gmail.com>
2015-12-28 23:57:37 +0600

    scroll to bottom on load of a user pending

--- app/custom_components/x-chat.js
+++ app/custom_components/x-chat.js
@@ -2,19 +2,21 @@ Polymer({
     is: 'chat-box',
     ready: function() {
         this.messages = [
-            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 1, message: 'From Pubnub1'},
-            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 2, message: 'From polymer'},
-            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 3, message: 'From polymer'},
-            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 4, message: 'From polymer'},
-            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 5, message: 'From polymer'},
-            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 6, message: 'From polymer'},
-            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 7, message: 'From polymer'},
-            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 8, message: 'From polymer'}
+            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 1, message: 'From Pubnub1', time: '7.29 AM'},
+            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 2, message: 'From polymer', time: '7.30 AM'},
+            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 3, message: 'From polymer', time: '7.31 AM'},
+            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 4, message: 'From polymer', time: '7.32 AM'},
+            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 5, message: 'From polymer', time: '7.33 AM'},
+            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 6, message: 'From polymer', time: '7.34 AM'},
+            {sender_id: 2,icon: 'polymer', sender: 'Polymer',message_id: 7, message: 'From polymer', time: '7.35 AM'},
+            {sender_id: 1,icon: 'cloud', sender: 'PubNub',message_id: 8, message: 'From polymer', time: '7.37 AM'}
         ];
     },
 
     classColor: function(sender_id) {
-        var logged_id = 1;
+        var logged_id = 2;
         return logged_id==sender_id? 'host':'remote';
     }
 });
+
+document.querySelector('#previous').scroller.scrollTop = 2000;
